0f6ca59105ab66bb3fcfdc66ff2d8273e9d743b7
Darren <Darren@LAPTOP-VJ1PVGUM>
2025-08-20 17:43:56 -0700

    Started implementation: added constructors.

--- Pair.cpp
+++ Pair.cpp
@@ -1,4 +1,19 @@
 // Pair.cpp
-// Your names
+// Christopher and Darren
 // Implement member functions here
 
+#include "Pair.h"
+
+// implementing default constructor
+template <typename T>
+Pair<T>::Pair() {
+   first = 0;
+   second = 0;
+}
+
+// implementing parameterized constructor
+template <typename T>
+Pair<T>::Pair(T f, T s) {
+   first = f;
+   second = s;
+}
