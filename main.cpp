9c6e893a6e1858138e313bdd342002d98f24ff68
Darren <Darren@LAPTOP-VJ1PVGUM>
2025-08-20 17:50:27 -0700

    Added test cases in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-// Your Names
+// Christopher and Darren
 #include "Pair.h"
+#include "Pair.cpp"
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // Add test cases here
-    return 0;
+   Pair<int> intConstructor(1, 2);
+   cout << "The sum of the two integers is " << intConstructor.getFirst() + intConstructor.getSecond()
+	    << "." << endl;
+
+   Pair<string> stringConstructor("apple", "banana");
+   cout << stringConstructor.getFirst() << " " << stringConstructor.getSecond() << endl;
+
+   return 0;
 }
